4f8cd9f7d9cb2005bd53314f79a22acdc8ba63f1
Jessica Gallo <jgallo2895@gmail.com>
2022-05-09 20:40:44 -0500

    completed inquirer prompts

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const promptUser = () => {
                 if (emailInput) {
                     return true;
                 } else {
-                    console.log("Please enter a valid ID!");
+                    console.log("Please enter a valid email!");
                     return false;
                 }
             }
@@ -57,7 +57,7 @@ const promptUser = () => {
                 if (officeInput) {
                     return true;
                 } else {
-                    console.log("Please enter a valid ID!");
+                    console.log("Please enter an office number!");
                     return false;
                 }
             }
@@ -68,5 +68,106 @@ const promptUser = () => {
         const manager = new Manager (name, id, email, office);
 
         teamArray.push(manager);
+    })
+};
+
+const promptEmployee = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is this employee's role?",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What's the name of the employee?", 
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter an employee's name!");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter a valid ID!");
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid email!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "Wha tis the employee's GitHub username?",
+            validate: githubInput =>  {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid username!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "Please enter the intern's school",
+            when: (input) => input.role === "Intern",
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                } else {
+                    console.log ("Please enter the intern's school!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmPromptEmployee',
+            message: 'Would you like to add more team members?',
+            default: false
+        }
+    ])
+    .then(employeeData => {
+        let { name, id, email, role, github, school, confrimPromptEmployee } = employeeData;
+        let employee;
+
+        if (role === "Engineer") {
+            employee = new Engineer (name, id, email, github);
+        } else if (role === "Intern") {
+            employee = new Intern (name, id, email, school);
+        }
+
+        teamArray.push(employee);
+
+        if (comfirmPromptEmployee) {
+            return promptEmployee(teamArray);
+        } else {
+            return teamArray;
+        }
     })
 };
