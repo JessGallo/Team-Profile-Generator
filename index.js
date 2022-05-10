073cbff2ffe4372bcf7a1505dab1937ed1233264
Jessica Gallo <jgallo2895@gmail.com>
2022-05-09 20:28:01 -0500

    added manager inquirer prompt

--- index.js
+++ index.js
@@ -1,9 +1,72 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const promptManager = require('./lib/Manager');
-const promptEmployee = require('./lib/Employee');
-const promptEngineer = require('./lib/Engineer');
-const promptIntern = require('./lib/Intern');
 
-promptManager()
-.then(promptEmployee)
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const teamArray = [];
+
+const promptUser = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of the manager?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the manager's name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's ID?",
+            validate: idInput => {
+                if (idInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid ID!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email?",
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid ID!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'office',
+            message: "What is the manager's office number?",
+            validate: officeInput => {
+                if (officeInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid ID!");
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(managerInput => {
+        const { name, id, email, office } = managerInput;
+        const manager = new Manager (name, id, email, office);
+
+        teamArray.push(manager);
+    })
+};
